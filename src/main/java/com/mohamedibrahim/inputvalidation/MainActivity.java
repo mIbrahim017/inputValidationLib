82b28800384b0c4a1454086960e77f0e2bd0e494
mohamed <mohammed2010_as@yahoo.com>
2016-07-20 13:09:15 +0200

    Delete MainActivity.java

--- src/main/java/com/mohamedibrahim/inputvalidation/MainActivity.java
+++
@@ -1,8 +0,0 @@
-package com.mohamedibrahim.inputvalidation;
-
-import com.mohamedibrahim.inputvalidation.Registration.RegistrationActivity;
-
-
-public class MainActivity extends RegistrationActivity {
-
-}
